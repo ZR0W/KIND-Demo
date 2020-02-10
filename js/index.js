9fb64b002a5a53ea7c31a989676975826aa0e7d8
mmanchen <mmanchen@u.rochester.edu>
2020-02-10 13:16:08 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@
 function message(){
     alert("bad words!");
 }
+function clearContents(element) {
+    element.value = '';
+  }
 
 document.getElementById("clickMe").onClick = "message()";
+document.getElementById("deleteMe").onClick = clearContents
