10f4327d4e6af7b658891a34238b0c67891d1e20
Rowland <rowlandz@outlook.com>
2020-02-11 19:57:45 -0500

    add popup modal box on button click and misc

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
 
 function message(){
-    alert("bad words!");
+  alert("bad words!");
 }
 function clearContents(element) {
-    element.value = '';
+  element.value = '';
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function()
+ {
+  
+  document.getElementById("clickMe").onclick = message;
+  document.getElementById("deleteMe").onclick = clearContents
+  var modal = document.getElementById("myModal");
+  document.getElementById("clickMe").onclick = btClick;
+  document.getElementsByClassName("close")[0].onclick = spanClick;
+  window.onclick = windowClick;
+
+  // When the user clicks the button, open the modal 
+  function btClick() {
+    modal.style.display = "block";
   }
 
-document.getElementById("clickMe").onClick = "message()";
-document.getElementById("deleteMe").onClick = clearContents
+  // When the user clicks on <span> (x), close the modal
+  function spanClick() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  function windowClick(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}, false);
